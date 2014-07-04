2987a587220188da561b0a1e0ebe8a569b830f69
Percila Njira <njirap@gmail.com>
2014-07-04 15:52:09 +0300

    populate phone number

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+os = Outlet.all
+os.each do |o|
+	o.delete
+end
 
-Outlet.find_or_create_by_name! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
-Outlet.find_or_create_by_name! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+os = OutletContact.all
+os.each do |o|
+	o.delete
+end
 
-outlet1 = Outlet.all[0].id
-outlet2 = Outlet.all[1].id
+# Outlet.find_or_create_by_name! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
+# Outlet.find_or_create_by_name! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+
+# outlet1 = Outlet.all[0].id
+# outlet2 = Outlet.all[1].id
 
 
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254722946639", priority: 1
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254712345635", priority: 2
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254722946639"
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254712345635"
 
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "2540203861961", priority: 1
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921", priority: 2
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "2540203861961"
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921"
