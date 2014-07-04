d2bcfeaa5fd250e3c850965b97d5e3bf28cbbb14
Percila Njira <njirap@gmail.com>
2014-07-04 12:19:36 +0300

    remove all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Outlet.create! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
-Outlet.create! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+outlets = Outlet.all
+outlets.each do |o|
+	o.delete
+end
 
-OutletContact.create! outlet_id: 1, phone_number: "254722946639", priority: 1
-OutletContact.create! outlet_id: 1, phone_number: "", priority: 2
+outletscontacts = OutletContact.all
+outletscontacts.each do |o|
+	o.delete
+end
 
-OutletContact.create! outlet_id: 2, phone_number: "2540203861961", priority: 1
-OutletContact.create! outlet_id: 2, phone_number: "", priority: 2
+
+# Outlet.create! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
+# Outlet.create! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+
+# OutletContact.create! outlet_id: 1, phone_number: "254722946639", priority: 1
+# OutletContact.create! outlet_id: 1, phone_number: "254712345635", priority: 2
+
+# OutletContact.create! outlet_id: 2, phone_number: "2540203861961", priority: 1
+# OutletContact.create! outlet_id: 2, phone_number: "254722784921", priority: 2
