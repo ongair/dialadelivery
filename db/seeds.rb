b3acbf2dbd12d9599f37d3b4e3d1dabfe52271dc
Percila Njira <njirap@gmail.com>
2014-07-04 11:01:51 +0300

    outlet contacts'

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 Outlet.create! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
 Outlet.create! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+
+OutletContact.create! outlet_id: 1, phone_number: "254722946639", priority: 1
+OutletContact.create! outlet_id: 1, phone_number: "", priority: 2
+
+OutletContact.create! outlet_id: 2, phone_number: "2540203861961", priority: 1
+OutletContact.create! outlet_id: 2, phone_number: "", priority: 2
