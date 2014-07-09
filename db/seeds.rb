b4afcfdd5ed49e486254439b014fbc16a92d937c
Percila Njira <njirap@gmail.com>
2014-07-09 10:36:20 +0300

    Seeding again

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@
 # 	o.delete
 # end
 
-Outlet.find_or_create_by_name! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
-Outlet.find_or_create_by_name! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
+# Outlet.find_or_create_by_name! name: "Pizza Inn Ngong Road", latitude: -1.299965, longitude: 36.789738
+# Outlet.find_or_create_by_name! name: "Pizza Inn Junction", latitude: -1.298225, longitude: 36.762011
 
 outlet1 = Outlet.all[0].id
 outlet2 = Outlet.all[1].id
@@ -27,8 +27,8 @@ Surburb.find_or_create_by_name_and_outlet_id! :outlet_id=>outlet2, :name=>"Jamuh
 
 
 
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254722946639"
-#OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254712345635"
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254722946639"
+# #OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet1, phone_number: "254712345635"
 
-OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921"
-#OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921"
+# OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921"
+# #OutletContact.find_or_create_by_phone_number_and_outlet_id! outlet_id: outlet2, phone_number: "254722784921"
