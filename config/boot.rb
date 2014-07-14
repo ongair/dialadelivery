8435d9748037cd77a12ab43777ea58d924a8dae4
Percila Njira <njirap@gmail.com>
2014-07-14 10:58:44 +0300

    Message

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,13 @@
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+BEGIN: 'Dial-A-Delivery'
+START: 'PIZZA'
+API_URL: 'http://app.ongair.im/api/v1/base/send'
+API_IMAGE_URL: 'http://app.ongair.im/api/v1/base/send_image'
+API_VCARD_URL: 'http://app.ongair.im/api/v1/base/send_contact'
+TOKEN: '889e574a6234107f244218decbf99560'
+SIZE_MEDIUM: 800
+SIZE_SMALL: 600
+SIZE_LARGE: 1000
+WELCOME_MESSAGE: 'Hallo Thank you for choosing Dial-a-Delivery'
+OUTLET_MESSAGE: 'Your nearest Dial-A-Delivery location near is'
+NO_OUTLET_MESSAGE: 'Sorry we do not yet have an outlet near'
+NO_SURBURB_MESSAGE: "Sorry Please share your location via WhatsApp"
