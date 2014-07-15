afa1cbc84e42a42c5b2c36142a633300c89439ac
Percila Njira <njirap@gmail.com>
2014-07-15 13:23:39 +0300

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,13 +27,13 @@ Dialadelivery::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
   config.assets.version = '1.0'
   config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
