6e7605602325fee7f34e7062a772ff49d675e2c7
Percila Njira <njirap@gmail.com>
2014-07-23 15:57:30 +0300

    Add message type again

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,13 +2,16 @@
 #
 # Table name: messages
 #
-#  id           :integer          not null, primary key
-#  text         :string(255)
-#  created_at   :datetime
-#  updated_at   :datetime
-#  customer_id  :integer
-#  message_type :string(255)
-#  image        :binary
+#  id                 :integer          not null, primary key
+#  text               :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  customer_id        :integer
+#  message_type       :string(255)
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Message < ActiveRecord::Base
