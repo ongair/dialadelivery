75da338251cee3f2d0fd3e4256ae8cb93bfcab4c
Percila Njira <njirap@gmail.com>
2014-07-24 12:09:32 +0300

    Send ]Image as a url

--- app/models/message.rb
+++ app/models/message.rb
@@ -33,8 +33,7 @@ class Message < ActiveRecord::Base
 				url = "#{ENV['API_URL']}/send"
 				response = HTTParty.post(url, body: params_config, debug_output: $stdout)
 			when "image"
-				#{ENV['BASE_URL']}
-				image_url = "#{image.url.split('?').first}"
+				image_url = "#{ENV['BASE_URL']}#{image.url.split('?').first}"
 				params_config['image'] = image_url
 				url = "#{ENV['API_URL']}/send_image"
 				response = HTTMultiParty.post(url, body: params_config, debug_output: $stdout)
